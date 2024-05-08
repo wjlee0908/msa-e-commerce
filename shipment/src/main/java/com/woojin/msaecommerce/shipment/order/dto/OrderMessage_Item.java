5dace5bcb29e029f374a9fce4c5d8cc729196a1b
Woojin Lee <dev.woojin.lee@gmail.com>
2024-05-08 21:42:08 +0900

    [feature] (shipment) modify order message

--- shipment/src/main/java/com/woojin/msaecommerce/shipment/order/dto/OrderMessage_Item.java
+++ shipment/src/main/java/com/woojin/msaecommerce/shipment/order/dto/OrderMessage_Item.java
@@ -1,6 +1,5 @@
-package com.woojin.msaecommerce.order.order.dto;
+package com.woojin.msaecommerce.shipment.order.dto;
 
-import com.woojin.msaecommerce.order.order.OrderItem;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,13 +15,4 @@ public class OrderMessage_Item {
     private Integer quantity;
 
     private Long productCouponId;
-
-    public static OrderMessage_Item fromOrderItem(OrderItem item) {
-        return OrderMessage_Item.builder()
-                .id(item.getId())
-                .productId(item.getProductId())
-                .quantity(item.getQuantity())
-                .productCouponId(item.getProductCouponId())
-                .build();
-    }
 }
